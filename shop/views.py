f0891a7574d370c3247257bb61a34a9dd860ab87
lkm <baeot@gtplus.co.kr>
2018-10-24 12:45:31 +0900

    shop app finished

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def item_list(request): 
+    qs = Item.objects.all() 
+    return render(request, 'shop/item_list.html', { 
+        'item_list': qs, 
+    })
