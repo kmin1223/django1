9ac9a1fae01689ff18fd085d9b099690596acc17
lkm <baeot@gtplus.co.kr>
2018-10-24 15:56:30 +0900

    model is_publish migrate

--- shop/admin.py
+++ shop/admin.py
@@ -3,8 +3,9 @@ from .models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-        list_display = ['pk', 'name', 'short_desc', 'price']
+        list_display = ['pk', 'name', 'short_desc', 'price', 'is_publish']
         list_display_links = ['name']
+        list_filter = ['is_publish'] 
         search_fields = ['name']
         def short_desc(self, item):
             return item.desc[:20]
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator 
+from django import forms 
+
+ 
+def max_length_10(value):
+         if len(value) > 10:
+             raise forms.ValidationError('10글자 이상 !!!')
+
+#max_length_10 = MaxLengthValidator(10) 
+ 
+
 
 class Item(models.Model):
      name = models.CharField(max_length=100) 
      desc = models.TextField(blank=True) 
      price = models.PositiveIntegerField() 
+     is_publish = models.BooleanField(default=False)
      created_at = models.DateTimeField(auto_now_add=True) 
      updated_at = models.DateTimeField(auto_now=True)
 
