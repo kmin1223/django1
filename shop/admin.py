364ba8b758b8686a2d62689d7fa39302df1e91e3
lkm <baeot@gtplus.co.kr>
2018-10-24 14:39:31 +0900

    shop, itemAdmin cutom real

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-        pass
+        list_display = ['pk', 'name', 'short_desc', 'price']
+        list_display_links = ['name']
+        search_fields = ['name']
+        def short_desc(self, item):
+            return item.desc[:20]
--- shop/models.py
+++ shop/models.py
@@ -6,3 +6,6 @@ class Item(models.Model):
      price = models.PositiveIntegerField() 
      created_at = models.DateTimeField(auto_now_add=True) 
      updated_at = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return self.name
